92d8eb5b71d1795efc52d63259e90d1043b62d3c
fand1l <fand1l@outlook.com>
2026-06-02 19:06:31 +0300

    Add JWT authentication configuration and update API URL routing

--- FinanceTrackerAPI/api_urls.py
+++ FinanceTrackerAPI/api_urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('finance/', include('finance.urls')),
     path('users/', include('users.urls')),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
