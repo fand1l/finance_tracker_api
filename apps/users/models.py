262b7b29f6bea9219a83e89d1bd48ba029a90b15
fand1l <fand1l@outlook.com>
2026-06-02 16:42:07 +0300

    Add CustomUser model with unique username and email fields

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=32, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.email}"
