c30fa07803c6b304f5ba3a9ad092c4fe26b8235d
fand1l <fand1l@outlook.com>
2026-06-02 19:06:47 +0300

    Register Category and Transaction models in admin with custom admin classes

--- FinanceTrackerAPI/finance/admin.py
+++ FinanceTrackerAPI/finance/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Transaction
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'type')
+    search_fields = ('user__username', 'user__email', 'name')
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'category', 'amount', 'date')
+    search_fields = ('user__username', 'user__email', 'amount')
+    list_filter = ('date', 'category__type', 'category__name')
+    list_select_related = ('user', 'category')
--- FinanceTrackerAPI/users/admin.py
+++ FinanceTrackerAPI/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email')
+    search_fields = ('username', 'email')
