4e5f2705e8dfac79c8c620216a5ca44abbc2ab58
fand1l <fand1l@outlook.com>
2026-06-03 19:23:20 +0300

    Implement Category and Transaction viewsets with authentication and user filtering

--- FinanceTrackerAPI/finance/views.py
+++ FinanceTrackerAPI/finance/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Category, Transaction
+from .serializers import CategorySerializer, TransactionSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user).select_related('category')
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
