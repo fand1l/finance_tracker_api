1617f468d55dab9e890672fcc8bc43c965d6761c
fand1l <fand1l@outlook.com>
2026-06-03 19:23:14 +0300

    Implement CustomUserViewSet with permission handling for user creation

--- FinanceTrackerAPI/users/views.py
+++ FinanceTrackerAPI/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django.conf import settings
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+class CustomUserViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomUserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
