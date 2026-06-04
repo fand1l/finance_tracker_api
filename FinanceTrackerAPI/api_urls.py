50f4d630c662bef2e5c53a5466ad2bd5129aea50
fand1l <fand1l@outlook.com>
2026-06-04 14:34:40 +0300

    Refactor API URL paths for user authentication and documentation

--- FinanceTrackerAPI/api_urls.py
+++ FinanceTrackerAPI/api_urls.py
@@ -1,15 +1,11 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('finance/', include('finance.urls')),
-    path('users/', include('users.urls')),
+    path('auth/', include('users.urls')),
 
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger_ui'),
+    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
